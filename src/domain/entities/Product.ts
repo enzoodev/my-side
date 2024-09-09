7adf6cd17eac116e593901f6929f1f50f0fbb5cc
Enzo Damascena <enzo.damascena@defensoria.mg.def.br>
2024-09-09 15:33:23 -0300

    feat: Add Product entity type

---
+++ src/domain/entities/Product.ts
@@ -0,0 +1,12 @@
+type TProduct = {
+  id: number
+  title: string
+  image: string
+  price: number
+  description: string
+  brand: string
+  model: string
+  color: string
+  category: string
+  discount: number
+}
