987ff94e3f8ddf88f720e3ce01ccfcbc6fa4d190
Enzo Damascena <enzo.damascena@defensoria.mg.def.br>
2024-09-09 16:27:51 -0300

    refactor: Update QueryKey get products key

--- src/enums/QueryKey.ts
+++ src/enums/QueryKey.ts
@@ -1,6 +1,5 @@
 export enum QueryKey {
-  GET_PRODUCTS_BY_PAGE = 'get-products-by-page',
-  GET_PRODUCTS_BY_CATEGORY = 'get-products-by-category',
+  GET_PRODUCTS = 'get-products',
   GET_PRODUCT_BY_ID = 'get-product-by-id',
   GET_ALL_CATEGORIES = 'get-all-categories',
 }
