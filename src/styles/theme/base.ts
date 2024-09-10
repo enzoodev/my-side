9289181903e1263b6beba9ec906bef4d53b83047
Enzo Damascena <enzo.damascena@defensoria.mg.def.br>
2024-09-10 10:57:14 -0300

    feat: Add base theme for styling

---
+++ src/styles/theme/base.ts
@@ -0,0 +1,41 @@
+import { BaseTheme } from '@/@types/styled'
+
+export const baseTheme: BaseTheme = {
+  fontSizes: {
+    xs: '12px',
+    sm: '14px',
+    md: '16px',
+    lg: '18px',
+    xl: '20px',
+  },
+  layout: {
+    1: '4px',
+    2: '8px',
+    3: '12px',
+    4: '16px',
+    5: '20px',
+    6: '24px',
+    7: '28px',
+    8: '32px',
+    9: '36px',
+    10: '40px',
+    12: '48px',
+    16: '64px',
+    20: '80px',
+    24: '96px',
+    28: '112px',
+    32: '128px',
+    40: '160px',
+    48: '192px',
+    56: '224px',
+    64: '256px',
+    72: '288px',
+    80: '320px',
+    96: '384px',
+  },
+  borderRadius: {
+    sm: '4px',
+    md: '8px',
+    lg: '16px',
+  },
+}
